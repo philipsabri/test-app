1e2156d9faa123042587ca93ffadf8f4288d3f0f
Philip Sabri <4808216+philipsabri@users.noreply.github.com>
2023-03-20 14:58:34 +0100

    Update hej.sh

--- hej.sh
+++ hej.sh
@@ -6,7 +6,7 @@
 #
 #git diff --name-only 4145b987a959d34114a2379a3610a2584788d497 HEAD # Ska inte trigga docker build
 #deployment/values-dev.yaml
-
+#
 
 CHANGED_FILES=$(git diff --name-only da90a1eba21a60503c88f201724c17b0e04cb3d7 HEAD)
 echo "$CHANGED_FILES" # For debugging purposes
